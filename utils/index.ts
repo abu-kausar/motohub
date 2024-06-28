cfde9ccbf2d64de73226ae879e78bf724d5e1dbd
abu-kausar <abukausar33@gmail.com>
2024-06-28 12:05:36 +0600

    custom filter and search part implemented

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars( filters : FilterProps ) {
+    const { manufacturer, year, fuel, model, limit } = filters;
+
     const headers = {
 		'x-rapidapi-key': 'd4bfe53d33mshc88c5a5dbe0aaaap1c209cjsncddc867c86c8',
 		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
 	} 
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit${limit}&fuel_type=${fuel}`, {
         headers: headers
     })
 
@@ -43,4 +45,14 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
     url.searchParams.append('angle', `${angle}`);
 
     return `${url}`;
+}
+
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    searchParams.set(type, value);
+
+    const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+
+    return newPathName;
 }
